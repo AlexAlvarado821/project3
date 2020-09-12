ec247061f38065a42d3702d785f8a6a9fba85b5f
AlexAlvarado821 <AAAlvaradoBarahon@dmacc.edu>
2020-09-12 18:39:00 -0500

    Initial commit

---
+++ module3/format_output/average_scores.py
@@ -0,0 +1,15 @@
+
+
+def average ():
+    score1 = input ("Enter your first grade: ")
+    score2 = input("Enter your second grade: ")
+    score3 = input("Enter your third grade: ")
+    return (int (score1) + int (score2) + int (score3))
+
+
+if __name__ == '__main__'
+    lastName = input ("Enter your last Name: ")
+    firstName = input ("Enter your first Name: ")
+    age = input("Enter your age: ")
+
+
